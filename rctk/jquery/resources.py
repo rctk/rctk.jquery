a3abf1379f7ffb5f290293d1ee3fe2a897b33f25
Ivo van der Wijk <github@in.m3r.nl>
2011-06-13 07:12:47 +0000

    json2 moved to rctk.core
    git-svn-id: https://rctk.googlecode.com/svn/rctk.jquery/trunk@559 286bb87c-ec97-11de-a004-2f18c49ebcc3

--- rctk/jquery/resources.py
+++ rctk/jquery/resources.py
@@ -4,7 +4,6 @@ addResource(JSFileResource('static/onion/onion.js'))
 addResource(JSFileResource('static/onion/core.js'))
 addResource(JSFileResource('static/onion/util.js'))
 addResource(JSFileResource('static/onion/jquery.js'))
-addResource(JSFileResource('static/json2.js'))
 addResource(CSSFileResource('static/onion/onion.css'))
 addResource(CSSFileResource('static/onion/onion.jqueryui.css'))
 
