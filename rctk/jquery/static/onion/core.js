7a93c80d4fb53c89596c436aeb557e508f0f2881
Ivo van der Wijk <github@in.m3r.nl>
2011-06-15 20:26:49 +0000

    remove last traces of old implementation
    git-svn-id: https://rctk.googlecode.com/svn/rctk.jquery/trunk@577 286bb87c-ec97-11de-a004-2f18c49ebcc3

--- rctk/jquery/static/onion/core.js
+++ rctk/jquery/static/onion/core.js
@@ -3,16 +3,18 @@ var rctk = rctk || {};
 rctk.jquery = (function($) {
   // return a functions so we can create instances using new
   return function() {
-    var o = null;
     var core = null;
 
+
     return {
+        root: $("#root"),
+        factory: $("#factory"),
+        toplevels: $("#toplevels"),
+
         run: function() {
             core = new rctk.core();
-            o = new Onion.core.JWinClient(core);
-            core.handlers.handle = rctk.util.proxy(o.do_work, o);
             core.handlers.request = rctk.util.proxy(this.rctk_request, this);
-            core.handlers.dump = rctk.util.proxy(o.dump, o);
+            core.handlers.dump = rctk.util.proxy(this.dump, this);
             core.handlers.construct = rctk.util.proxy(this.construct, this);
             var root = new Onion.widget.Root(this);
             root.create();
@@ -32,55 +34,46 @@ rctk.jquery = (function($) {
         },
         construct: function(class, parent, id) {
             var control_class = Onion.widget.map(class);
-            c = new control_class(o, parent, id);
+            c = new control_class(this, parent, id);
             return c;
+        },
+        dump: function(data, debug) {
+            if(debug) {
+                this.root.append('<div id="system" class="jqmWindow" style="width: 600px; height: 600px"><b>The application ' + data.application + ' has crashed. </b><br><p>Click <a href="/">here</a> to restart</p><br><div style="overflow: auto; width: 600px; height: 500px;">' + data.traceback + '</div></div>');
+            }
+            else {
+                this.root.append('<div id="system" class="jqmWindow" style="width: 600px; height: 300px"><b>The application ' + data.application + ' has crashed. </b><br><p>Click <a href="/">here</a> to restart</p></div>');
+
+            }
+            $("#system").jqm({'modal':true});
+            $("#system").jqmShow();
+            return;
+
+        },
+
+        register_busy: function(c) {
+            rctk.util.log("Deprecated jwin register_busy called");
+            core.register_busy(c);
+        },
+
+        get_control: function(id) {
+            rctk.util.log("Deprecated jwin get_control called");
+            return core.get_control(id);
+        },
+
+        add_task: function(method, type, id, data) {
+            rctk.util.log("Deprecated jwin add_task called");
+            core.push({'method':method, 'type':type, 'id':id, 'data':data});
+        },
+
+        flush: function() {
+            // widgets call this to flush through self.jwin
+            // completely replacing self.jwin with rctk.core not an option yet
+            // due to busy registration
+            rctk.util.log("Deprecated JWinClient.flush called");
+            core.flush();
         }
     }
   }
 })(jQuery);
 
-Onion.core.JWinClient = function(core) {
-    this.core = core;
-
-    // references to the actual div's
-    this.root = $("#root");
-    this.factory = $("#factory");
-    this.toplevels = $("#toplevels");
-}
-
-Onion.core.JWinClient.prototype.dump = function(data, debug) {
-    if(debug) {
-        this.root.append('<div id="system" class="jqmWindow" style="width: 600px; height: 600px"><b>The application ' + data.application + ' has crashed. </b><br><p>Click <a href="/">here</a> to restart</p><br><div style="overflow: auto; width: 600px; height: 500px;">' + data.traceback + '</div></div>');
-    }
-    else {
-        this.root.append('<div id="system" class="jqmWindow" style="width: 600px; height: 300px"><b>The application ' + data.application + ' has crashed. </b><br><p>Click <a href="/">here</a> to restart</p></div>');
-
-    }
-    $("#system").jqm({'modal':true});
-    $("#system").jqmShow();
-    return;
-
-}
-
-Onion.core.JWinClient.prototype.register_busy = function(c) {
-    rctk.util.log("Deprecated jwin register_busy called");
-    this.core.register_busy(c);
-}
-
-Onion.core.JWinClient.prototype.get_control = function(id) {
-    rctk.util.log("Deprecated jwin get_control called");
-    return this.core.get_control(id);
-}
-
-Onion.core.JWinClient.prototype.add_task = function(method, type, id, data) {
-    rctk.util.log("Deprecated jwin add_task called");
-    this.core.push({'method':method, 'type':type, 'id':id, 'data':data});
-}
-
-Onion.core.JWinClient.prototype.flush = function() {
-    // widgets call this to flush through self.jwin
-    // completely replacing self.jwin with rctk.core not an option yet
-    // due to busy registration
-    rctk.util.log("Deprecated JWinClient.flush called");
-    this.core.flush();
-}
