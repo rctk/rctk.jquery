b31a7a8d8879b0f7820be517582c9361aed126f6
Ivo van der Wijk <github@in.m3r.nl>
2011-08-21 11:17:07 +0000

    resource reorganization
    git-svn-id: https://rctk.googlecode.com/svn/rctk.jquery/trunk@614 286bb87c-ec97-11de-a004-2f18c49ebcc3

--- rctk/jquery/resources.py
+++ rctk/jquery/resources.py
@@ -1,10 +1,10 @@
 from rctk.resourceregistry import addResource, JSFileResource, CSSFileResource
 
 addResource(JSFileResource('static/jqueryui-1.8.4/jquery-ui-1.8.4.custom.min.js'))
-addResource(JSFileResource('static/onion/onion.js'))
-addResource(JSFileResource('static/onion/core.js'))
-addResource(CSSFileResource('static/onion/onion.css'))
-addResource(CSSFileResource('static/onion/onion.jqueryui.css'))
+addResource(JSFileResource('core/onion.js'))
+addResource(JSFileResource('core/core.js'))
+addResource(CSSFileResource('core/onion.css'))
+addResource(CSSFileResource('core/onion.jqueryui.css'))
 
 import rctk.jquery.layouts.resources
 import rctk.jquery.widgets.resources
