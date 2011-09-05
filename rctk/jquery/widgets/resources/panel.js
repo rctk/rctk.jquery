843a99662f2e77ccef4b047b1872386cbdd0d84a
Ivo van der Wijk <github@in.m3r.nl>
2011-09-05 13:03:06 +0000

    scrollpane update, scrolling works again. Support for explicit top/bottom scrolling
    git-svn-id: https://rctk.googlecode.com/svn/rctk.jquery/trunk@670 286bb87c-ec97-11de-a004-2f18c49ebcc3

--- rctk/jquery/widgets/resources/panel.js
+++ rctk/jquery/widgets/resources/panel.js
@@ -15,7 +15,7 @@
 Onion.widget.Panel = function(jwin, parent, controlid) {
     Onion.widget.Container.apply(this, arguments);
     this.name = "panel";
-}
+};
 
 Onion.widget.Panel.prototype = new Onion.widget.Container();
 
@@ -25,34 +25,30 @@ Onion.widget.Panel.prototype.create = function(data) {
     if(data.scrolling) {
         this.scrolling = true;
 
-        this.control.jScrollPane();
-        this.container = this.control;
-        this.control = this.control.parent();
+        this.control.jScrollPane({maintainPosition:true});
+        this.api = this.control.data('jsp');
+        this.container = this.api.getContentPane();
     }
     else {
         this.scrolling = false;
     }
-}
+};
 
 Onion.widget.Panel.prototype.update_scrolling = function() {
     if(this.scrolling) {
-        this.container.jScrollPane();
+        this.api.reinitialise();
     }
-}
+};
 
 Onion.widget.Panel.prototype.append = function(control, data) {
     Onion.widget.Container.prototype.append.apply(this, arguments);
-    // perhaps this is also/already handled by jScrollPane
-    this.control.scrollTop(this.control.attr("scrollHeight"));
     this.update_scrolling();
-}
+};
 
 Onion.widget.Panel.prototype.layout_updated = function() {
     Onion.widget.Container.prototype.layout_updated.apply(this, arguments);
-    // perhaps this is also/already handled by jScrollPane
-    this.control.scrollTop(this.control.attr("scrollHeight"));
     this.update_scrolling();
-}
+};
 
 Onion.widget.Panel.prototype.max_size = function() {
     /*
@@ -60,11 +56,31 @@ Onion.widget.Panel.prototype.max_size = function() {
      * no scrollbar may appear
      */
     if(this.scrolling) {
-        return {width:this.maxwidth, height:0}
+        return {width:this.maxwidth, height:0};
     }
     else {
-        return {width:this.maxwidth, height:this.maxheight}
+        return {width:this.maxwidth, height:this.maxheight};
     }
-}
+};
+
+Onion.widget.Panel.prototype.set_properties = function(data) {
+    Onion.widget.Container.prototype.set_properties.apply(this, arguments);
+
+    if(this.scrolling && 'scrollto' in data) {
+        if(data.scrollto == "top") {
+            this.api.scrollToPercentY(0);
+        }
+        else if(data.scrollto == "bottom") {
+            this.api.scrollToPercentY(1);
+        }
+        else if(data.scrollto == "left") {
+            this.api.scrollToPercentX(0);
+        }
+        else if(data.scrollto == "right") {
+            this.api.scrollToPercentX(1);
+        }
+    }
+    // ignore scrolling, it can only be set at create
+};
 
 Onion.widget.register("panel", Onion.widget.Panel);
